5fae9618b58868b47da398b96d1d6ae7d031a0d0
ivamly <ivamly.dev@gmail.com>
2026-05-18 23:10:42 +0300

    Initial commit

---
+++ src/main/java/ru/ivamly/chill/ChillApplication.java
@@ -0,0 +1,13 @@
+package ru.ivamly.chill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ivamly/chill/ChillApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ivamly.chill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
